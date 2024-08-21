80cc5b894dc12fa885b406281fc80799fc460115
Ika <rexar.ika@gmail.com>
2024-08-21 17:04:39 +0300

    Comparator

--- collections/src/test.java
+++ collections/src/test.java
@@ -1,34 +1,41 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class test {
     public static void main(String[] args) {
-        MyLinkedList myLinkedList = new MyLinkedList();
-        myLinkedList.add(1);
-        myLinkedList.add(2);
-        myLinkedList.add(3);
-        System.out.println(myLinkedList);
+        List<String> animals = new ArrayList<>();
 
-        List<Integer> linkedList = new LinkedList<>();
-        List<Integer> arrayList = new ArrayList<>();
+        animals.add("dog");
+        animals.add("cat");
+        animals.add("frog");
+        animals.add("bird");
+        animals.add("horse");
+        animals.add("pig");
 
-        measureTime(linkedList);
-        measureTime(arrayList);
+        Collections.sort(animals, new StringLengthComparator());
+        System.out.println(animals);
+
+        List<Integer> numbers = new ArrayList<>();
+
+        numbers.add(5);
+        numbers.add(500);
+        numbers.add(23);
+        numbers.add(165);
+        numbers.add(52);
+
+        Collections.sort(numbers);
+        System.out.println(numbers);
     }
 
-    private static void measureTime(List<Integer> list) {
-        for (int i = 0; i < 100000; i++) {
-            list.add(0, i);
-        }
-        long start = System.currentTimeMillis();
+}
 
-        for (int i = 0; i < 100000; i++) {
-            list.remove(0);
-        }
-
-        long end = System.currentTimeMillis();
-
-        System.out.println(end - start);
+class StringLengthComparator implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        if(o1.length() > o2.length()) return 1;
+        else if (o1.length() < o2.length()) return -1;
+        else return 0;
     }
 }
