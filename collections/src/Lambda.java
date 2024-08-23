88442f774ea1bbb2f1a940ae529fe0fd27c3046e
Ika <rexar.ika@gmail.com>
2024-08-23 19:24:04 +0300

    lambda_final

--- collections/src/Lambda.java
+++ collections/src/Lambda.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 interface Executable {
     void execute();
 }
@@ -15,6 +20,44 @@ public class Lambda {
 
         Runner runner = new Runner();
         runner.run(() -> System.out.println("hello"));
+
+        List<Integer> list1 = new ArrayList<>();
+        int[] arr1 = new int[10];
+
+        fillArr(arr1);
+        fillList(list1);
+
+        arr1 = Arrays.stream(arr1).map(a -> a * 2).toArray();
+        list1 = list1.stream().map(a -> a * 2).toList();
+
+        arr1 = Arrays.stream(arr1).map(a -> 3).toArray();
+
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(list1);
+
+        int[] arr2 = new int[10];
+        List<Integer> list2 = new ArrayList<>();
+
+        fillList(list2);
+        fillArr(arr2);
+
+        arr2 = Arrays.stream(arr2).filter(a -> a % 2 == 0).toArray();
+        list2 = list2.stream().filter(a -> a % 2 == 0).collect(Collectors.toList());
+
+        System.out.println(Arrays.toString(arr2));
+        System.out.println(list2);
+    }
+
+    private static void fillList(List<Integer> list) {
+        for (int i = 0; i < 10; i++) {
+            list.add(i + 1);
+        }
+    }
+
+    private static void fillArr(int[] arr) {
+        for (int i = 0; i < 10; i++) {
+            arr[i] = i + 1;
+        }
     }
 }
 
